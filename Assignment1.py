86531d71d1ae7c1d88c1ef589eca09bf4f47bae1
sheejashibbu <sheejashibbu@gmail.com>
2024-12-13 11:36:38 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,40 @@
+name_1=input("Enter your name:")
+print("my name is", name_1)
+student_No=input("Enter Student No:")
+print ("Student No:",student_No)
+email=input("enter email id")
+print("email id:",email)
+print("My name is",name_1,"\nstudent No:",student_No,"\nemail id :",email )
+num1= int(input("Enter first No"))
+num2= int(input("enter Second No:"))
+add=num1+num2
+sub=num1-num2
+mult=num1*num2
+div=num1/num2
+print("addition=", add, "subtraction=",sub, "multiplication=", mult, " division=w",div)
+print("1","2","3","4","5")
+print (' "SDK" stands for "Software Development Kit", whereas "IDE" stands for "Integrated Development Environment"')
+print("python is an \"awesome\" language.")
+print("python\n\t2023")
+print('I\'m from Entri.\b')
+print("\65")
+print("\x65")
+print("Entri", "2023", sep="\n")
+print("Entri", "2023", sep="\b")
+print("Entri", "2023", sep="*", end="\b\b\b\b")
+days=365
+hours=24
+minute=60
+print ("No: of days in a year=",days)
+print("No of hours in a day=", hours)
+print("No of minutes in in an hour=", minute)
+total_no_minute=days*hours*minute
+print("Total number of minutes in an year=",total_no_minute)
+name=input(" Enter Your Name")
+print("Hi",name,"welcome to Python programming")
+pound=float(input("Enter am amount in pound:"))
+dollars=1.27*pound
+print(pound,"£","are",dollars,"$")
+
+
+
